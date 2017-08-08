b709f40539361b3580f915d6c67f1da651e69aca
Ian You <jungyou87@gmail.com>
2017-08-08 16:14:31 -0500

    Dojo Dashboard II

--- ian_you/rails/Dojo_Dashboard/dojo_dashboard/app/controllers/dojos_controller.rb
+++ ian_you/rails/Dojo_Dashboard/dojo_dashboard/app/controllers/dojos_controller.rb
@@ -1,6 +1,30 @@
 class DojosController < ApplicationController
   def index
+    flash[:success] ||= []
     @dojos = Dojo.all
     return render 'dojos/index.html.erb'
   end
+
+  def new
+    flash[:message] ||= []
+    return render 'dojos/new.html.erb'
+  end
+
+  def create
+    @dojo = Dojo.create(dojo_params)
+    
+    if  @dojo.errors.full_messages != [] 
+      flash[:message] = @dojo.errors.full_messages
+      return redirect_to '/dojos/new'
+    
+    else
+      flash[:success] ="Successfully created" 
+      return redirect_to '/dojos'
+    end
+  end
+
+  private
+    def dojo_params
+      params.require(:dojo).permit(:branch, :street, :city, :state)
+    end
 end
